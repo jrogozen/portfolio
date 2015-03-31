fa8426a10610b5e16c41d7117fb25ee7eff9980d
Jon Rogozen <jrogozen@hotpads.com>
2015-03-31 11:37:12 -0700

    add production

--- gulpfile.js
+++ gulpfile.js
@@ -32,10 +32,17 @@ gulp.task('inject', function() {
 });
 
 gulp.task('server', function() {
-  gulp.src('./')
-    .pipe(webserver({
-      livereload: true
-    }))
+  if (NODE_ENV === 'production') {
+    gulp.src('./')
+      .pipe(webserver({
+        host: 'jonrogozen.com'
+      }))
+  } else {
+    gulp.src('./')
+      .pipe(webserver({
+        livereload: true
+      }))
+  }
 });
 
 gulp.task('watch', function() {
